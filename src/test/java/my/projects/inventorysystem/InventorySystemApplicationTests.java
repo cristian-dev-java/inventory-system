2a2795902e694860c8bbf4b8b2ca6fe7879124ae
DESKTOP-O8JE48T\Criss <cristiancamilomunozarango@gmail.com>
2023-01-20 19:58:56 -0500

    Se sube arquetipo del proyecto

---
+++ src/test/java/my/projects/inventorysystem/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.inventorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
